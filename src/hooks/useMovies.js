c5039c9ba788309071dba35f6bc183cbbd493f77
Roenan Halili <haliliroenan12@gmail.com>
2024-07-13 16:17:56 +0800

    create custom hook useMovies

---
+++ src/hooks/useMovies.js
@@ -0,0 +1,59 @@
+import { useState, useEffect } from "react";
+import { KEY } from "../App";
+
+export function useMovies(query, callback) {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(
+    function () {
+      //call the function if it exists (handleCloseMovie)
+      callback?.();
+
+      const controller = new AbortController(); //native browser api
+
+      async function fetchMovies() {
+        try {
+          setIsLoading(true);
+          setError("");
+          const res = await fetch(
+            `http://www.omdbapi.com/?s=${query}&apikey=${KEY}`,
+            { signal: controller.signal }
+          );
+
+          if (!res.ok)
+            throw new Error("Something went wrong with fetching movies");
+
+          const data = await res.json();
+          if (data.Response === "False") throw new Error("Movie not found");
+          setMovies(data.Search);
+          setError("");
+        } catch (err) {
+          //ignore the abort error
+          if (err.name !== "AbortError") {
+            console.log(err.message);
+            setError(err.message);
+          }
+        } finally {
+          setIsLoading(false);
+        }
+      }
+
+      if (query.length < 3) {
+        setMovies([]);
+        setError("");
+        return;
+      }
+
+      fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
+    },
+    [query, callback]
+  );
+
+  return { movies, isLoading, error };
+}
