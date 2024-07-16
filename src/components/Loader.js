a177c6c57ece9eaf04b9bee749559daecd8b6395
Roenan Halili <haliliroenan12@gmail.com>
2024-07-16 22:00:13 +0800

    add css loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,3 +1,12 @@
 export function Loader() {
-  return <p className="loader">Loading...</p>;
+  return (
+    <div class="wrapper loader">
+      <div class="circle"></div>
+      <div class="circle"></div>
+      <div class="circle"></div>
+      <div class="shadow"></div>
+      <div class="shadow"></div>
+      <div class="shadow"></div>
+    </div>
+  );
 }
